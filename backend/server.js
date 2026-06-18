54ef0872e64e1e3fe24155c383ede515d097b543
thenikhilsingh <nikhilsingh26042004@gmail.com>
2026-06-18 10:42:58 +0530

    connect signup page to api and solve cors error

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,17 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
 const indexRouter = require("./routes/indexRouter");
 const authRouter = require("./routes/authRouter.js");
+const cors = require("cors");
 dotenv.config();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use("/", indexRouter);
 app.use("/api/auth", authRouter);
--- frontend/pages/Signup.jsx
+++ frontend/pages/Signup.jsx
@@ -1,7 +1,44 @@
-import { Link } from "react-router-dom";
-import { CloudUpload, Eye } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { CloudUpload, Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Signup() {
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        `${API_BASE_URL}/api/auth/signup`,
+        formData,
+      );
+      console.log(response.data);
+      if (response.status === 201) {
+        setFormData({
+          name: "",
+          email: "",
+          password: "",
+        });
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gray-50 flex items-center justify-center px-4 py-8">
       <div className="w-full max-w-5xl bg-white rounded-2xl shadow-lg flex flex-col md:flex-row overflow-hidden">
@@ -25,13 +62,16 @@ export default function Signup() {
           <div className="w-full max-w-md border border-gray-200 rounded-xl p-8 shadow-sm">
             <h2 className="text-2xl font-semibold mb-6">Signup</h2>
 
-            <form className="space-y-5">
+            <form className="space-y-5" onSubmit={handleSubmit}>
               <div>
                 <label className="block text-sm font-medium mb-2">Name</label>
                 <input
                   type="text"
                   placeholder="Enter your name"
                   className="w-full border rounded-lg px-4 py-3 outline-none focus:border-blue-500"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
                 />
               </div>
 
@@ -41,6 +81,9 @@ export default function Signup() {
                   type="email"
                   placeholder="Enter your email"
                   className="w-full border rounded-lg px-4 py-3 outline-none focus:border-blue-500"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
                 />
               </div>
 
@@ -51,16 +94,20 @@ export default function Signup() {
 
                 <div className="relative">
                   <input
-                    type="password"
+                    type={showPassword ? "text" : "password"}
                     placeholder="Enter your password"
                     className="w-full border rounded-lg px-4 py-3 pr-12 outline-none focus:border-blue-500"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
                   />
 
                   <button
                     type="button"
                     className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400"
+                    onClick={() => setShowPassword(showPassword ? false : true)}
                   >
-                    <Eye size={18} />
+                    {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
                   </button>
                 </div>
               </div>
