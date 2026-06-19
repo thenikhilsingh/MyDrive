d60488629bc0d8b48428589797f44233d1648e1e
thenikhilsingh <nikhilsingh26042004@gmail.com>
2026-06-19 17:01:56 +0530

    created upload file controller

--- backend/controllers/fileController.js
+++ backend/controllers/fileController.js
@@ -10,4 +10,34 @@ const getFiles = async (req, res) => {
   }
 };
 
-module.exports = { getFiles };
+const uploadFile = async (req, res) => {
+  try {
+    // Check file exists
+    if (!req.file) {
+      return res.status(400).json({
+        message: "Please upload a file",
+      });
+    }
+
+    const newFile = await File.create({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+      fileUrl: `/uploads/${req.file.filename}`, //local file path
+      publicId: req.file.filename, //temporary for local storage
+      uploadedBy: req.user._id,
+      folder: req.body.folderId,
+    });
+
+    res.status(201).json({
+      message: "File uploaded successfully",
+      file: newFile,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = { getFiles, uploadFile };
--- backend/routes/fileRouter.js
+++ backend/routes/fileRouter.js
@@ -1,10 +1,11 @@
 const Router = require("express");
 const authMiddleware = require("../middlewares/authMiddleware.js");
-const { getFiles } = require("../controllers/fileController.js");
+const { getFiles, uploadFile } = require("../controllers/fileController.js");
+const upload = require("../middlewares/multerMiddleware.js");
 
 const fileRouter = Router();
 
 fileRouter.get("/", authMiddleware, getFiles);
-fileRouter.get("/upload", authMiddleware);
+fileRouter.post("/upload", authMiddleware, upload.single("file"), uploadFile);
 
 module.exports = fileRouter;
