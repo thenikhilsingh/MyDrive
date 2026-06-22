6ecc97aee429745753c8f8efe965b5fda4bd66be
thenikhilsingh <nikhilsingh26042004@gmail.com>
2026-06-22 20:06:06 +0530

    fixed a bug

--- backend/controllers/folderController.js
+++ backend/controllers/folderController.js
@@ -2,7 +2,7 @@ const Folder = require("../models/folder");
 const File = require("../models/file.js");
 const fs = require("fs");
 const path = require("path");
-const cloudinary = require("../utils/cloudinary.js");
+const { cloudinary } = require("../utils/cloudinary.js");
 
 const getFolders = async (req, res) => {
   try {
